c5092403b03e8d7cd11ba1da1507df4d031522e9
maddy <maddy.fries@protonmail.com>
2022-02-18 18:46:17 -0800

    uhh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
-import Image from "next/Image";
+import Image from "next/image";
 import Link from 'next/link';
 
 // const GirlGlasses: HTMLImageElement = require('../images/oladimeji-odunsi-Wu3yqve2gnc-unsplash-cropped.jpg');
--- src/pages/models.tsx
+++ src/pages/models.tsx
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
-import Image from "next/Image";
+import Image from "next/image";
 import Link from "next/link";
 
 import FarmGirl from "../images/scroll/farmgorl.jpg";
